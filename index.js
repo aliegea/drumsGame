c2114c140336b55b68f1adbcf47ffc2d8827c08e
Aliciaegea <aliegea4@gmail.com>
2021-12-29 23:57:24 +0100

    creating keydown function and animations

--- index.js
+++ index.js
@@ -3,5 +3,20 @@ function handleclick(e) {
   let letter = e.target.innerText;
   let audio = new Audio("sounds/" + letter + ".mp3");
   audio.play();
+
+  buttonAnimation(letter);
 }
 drumsNum.forEach((element) => element.addEventListener("click", handleclick));
+document.addEventListener("keydown", function (e) {
+  audio = new Audio("sounds/" + e.key + ".mp3");
+  audio.play();
+  buttonAnimation(e.key);
+});
+
+function buttonAnimation(key) {
+  let activeBtn = document.querySelector("." + key);
+  activeBtn.classList.add("pressed");
+  setTimeout(() => {
+    activeBtn.classList.remove("pressed");
+  }, 500);
+}
