8e4feed554b555e46a89f85f4e46cf146d215ce3
Aliciaegea <aliegea4@gmail.com>
2021-12-29 18:16:56 +0100

    refactoring handleclick function code

--- index.js
+++ index.js
@@ -1,38 +1,7 @@
 let drumsNum = document.querySelectorAll(".drum");
 function handleclick(e) {
   let letter = e.target.innerText;
-
-  switch (letter) {
-    case "w":
-      let tom1 = new Audio("sounds/w.mp3");
-      tom1.play();
-      break;
-    case "a":
-      let tom2 = new Audio("sounds/tom-2.mp3");
-      tom2.play();
-      break;
-    case "s":
-      let tom3 = new Audio("sounds/tom-3.mp3");
-      tom3.play();
-      break;
-    case "d":
-      let tom4 = new Audio("sounds/tom-4.mp3");
-      tom4.play();
-      break;
-    case "j":
-      let snare = new Audio("sounds/snare.mp3");
-      snare.play();
-    case "k":
-      let crash = new Audio("sounds/crash.mp3");
-      crash.play();
-      break;
-    case "l":
-      let kick = new Audio("sounds/kick-bass.mp3");
-      kick.play();
-      break;
-
-    default:
-      break;
-  }
+  let audio = new Audio("sounds/" + letter + ".mp3");
+  audio.play();
 }
 drumsNum.forEach((element) => element.addEventListener("click", handleclick));
